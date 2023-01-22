a58582a65f0a96284e31b27b9ddb070ff16c97ed
ZoePlayoust <100842308+ZoePlayoust@users.noreply.github.com>
2023-01-22 15:01:16 +0100

    Add new topic action

---
+++ src/features/topics/TopicsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const options = {
+  name: "topics",
+  initialState: {
+    topics: {}
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      const { id, name, icon } = action.payload;
+      state.topics[id] = {
+        id: id,
+        name: name,
+        icon: icon,
+        quizIds: []
+      };
+    },
+   
+  },
+  // extra reducers here
+};
+
+export const topicsSlice = createSlice(options);
+// selectors
+export const selectTopics = (state) => state.topics.topics;
+// actions + reducers
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
